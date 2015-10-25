795c61016e6ccf0d5805a368ff5c685c98c89bf0
Alexey Dubinin <dubininalexey@gmail.com>
2015-10-26 00:05:23 +0300

    Build

--- out/tabun-fixes.user.js
+++ out/tabun-fixes.user.js
@@ -250,6 +250,11 @@ define(['module', 'deep', 'require'], function(Module, deep, require) {
         return this._enabled[this._getModuleId(moduleOrId)]
     }
 
+    App.prototype.isModuleDirty = function app_isModuleDirty(moduleOrId) {
+        this._checkStarted(true)
+        return this._dirty[this._getModuleId(moduleOrId)]
+    }
+
     App.prototype.setModuleEnabled = function app_setModuleEnabled(moduleOrId, enabled) {
         this._checkStarted(true)
 
@@ -609,7 +614,7 @@ define(['jquery', 'deep', 'cfg-panel-applet'], function($, deep, CfgPanelApplet)
 
         this._ui.find('INPUT[name],TEXTAREA[name]').each(function() {
             var el = $(this)
-            res[el.attr('name')] = getval(el)
+            res[el.attr('name')] = getVal(el)
         })
 
         return res
@@ -776,6 +781,7 @@ define(['jquery', 'module', 'app', 'basic-cfg-panel-applet', 'img/gear'], functi
                     module: applet.module,
                     ui: $(applet.applet.build())
                             .css('margin-bottom', '10px')
+                            .css('background', applet.module.isDirty() ? '#FDD' : 'none') // TODO : theme
                             .appendTo(td),
                 }
             }.bind(this))
@@ -835,8 +841,6 @@ define(['jquery', 'module', 'app', 'basic-cfg-panel-applet', 'img/gear'], functi
         $(document.body).append(this._dialog)
 
         this._setAppletsData()
-
-        // TODO : highlight dirty modules
     }
 
     CfgPanel.prototype._setAppletsData = function cfgPanel_setAppletsData() {
@@ -883,6 +887,104 @@ define(['jquery', 'module', 'app', 'basic-cfg-panel-applet', 'img/gear'], functi
 
 })
 
+define('reveal-lite-spoilers', [])
+define(['module', 'basic-cfg-panel-applet'], function(Module, BasicCfgPanelApplet) {
+
+    function RevealLiteSpoilersModule() { }
+
+    RevealLiteSpoilersModule.prototype = new Module()
+
+    RevealLiteSpoilersModule.prototype.init = function revealLiteSpoilers_init(config) {
+        config = config || {
+            alwaysReveal: false,
+        }
+        return config
+    }
+
+    RevealLiteSpoilersModule.prototype.getLabel = function revealLiteSpoilers_getLabel() {
+        return "Приоткрывать лайт-спойлеры при наведении на пост/коммент"
+    }
+
+    RevealLiteSpoilersModule.prototype.attach = function revealLiteSpoilers_attach(config) {
+        console.log('attach', this._style)
+        this._generateStyleSheet(config)
+        this._style.appendTo(document.head)
+    }
+
+    RevealLiteSpoilersModule.prototype.detach = function revealLiteSpoilers_detach(сonfig) {
+        console.log('detach', this._style)
+        this._style.remove()
+        this._style = null
+
+        return true
+    }
+
+    RevealLiteSpoilersModule.prototype.createCfgPanelApplet = function revealLiteSpoilers_createCfgPanelApplet() {
+        var chkAlways = $('<input>')
+                .attr('type', 'checkbox')
+                .attr('name', 'alwaysReveal')
+
+        return new BasicCfgPanelApplet(this.getLabel(),
+                $("<br/>"),
+                $('<label>').text("Всегда светить лайт-спойлеры").prepend(chkAlways))
+    }
+
+    RevealLiteSpoilersModule.prototype._generateStyleSheet = function revealLiteSpoilers_generateStyleSheet(config) {
+        // http://userstyles.org/styles/92211/night-tabun
+        var nightTabun = getComputedStyle($('<span>').attr('class', 'spoiler-gray')[0]).backgroundColor == "rgb(63, 53, 61)"
+
+        // always visible state
+        var transBgColor           = nightTabun ? '#2F252D' : '#EEE'
+        var transTextColor         = nightTabun ? '#8F8F8F' : '#999'
+        var transATextColor        = nightTabun ? '#7C89CA' : '#66AAFF'
+        var transAVisitedTextColor = nightTabun ? '#7C89CA' : '#66AAFF'
+
+        // hover state (fully visible)
+        var hoverTextColor         = nightTabun ? '#DFDFDF' : '#666'
+        var hoverATextColor        = nightTabun ? '#7C89CA' : '#0099FF'
+        var hoverAVisitedTextColor = nightTabun ? '#7C89CA' : '#0099FF'
+
+        var containers = ['.comment', '.comment-preview', '.topic', '.profile-info-about']
+            // селекторы для спойлеров в обычном состоянии
+          , selectorSpoiler = containers.map(function(s) { return s + ' .spoiler-gray' }).join(', ')
+          , selectorA = containers.map(function(s) { return s + ' .spoiler-gray A' }).join(', ')
+          , selectorAVisited = containers.map(function(s) { return s + ' .spoiler-gray A:visited' }).join(', ')
+            // селекторы для наведённого коммента/поста
+          , selectorPostHoverSpoiler = containers.map(function(s) { return s + ':hover .spoiler-gray' }).join(', ')
+          , selectorPostHoverA = containers.map(function(s) { return s + ':hover .spoiler-gray A' }).join(', ')
+          , selectorPostHoverAVisited = containers.map(function(s) { return s + ':hover .spoiler-gray A:visited' }).join(', ')
+            // и более специфичные селекторы для оригинального лайтспойлера в наведённом состоянии (иначе эти стили не пробиваются через наши)
+          , selectorHoverSpoiler = containers.map(function(s) { return s + ':hover .spoiler-gray:hover' }).join(', ')
+          , selectorHoverA = containers.map(function(s) { return s + ':hover .spoiler-gray:hover A' }).join(', ')
+          , selectorHoverAVisited = containers.map(function(s) { return s + ':hover .spoiler-gray:hover A:visited' }).join(', ')
+
+        if (config.alwaysReveal) {
+            this._style = $('<style>').text(
+                selectorSpoiler + ' { background-color: ' + transBgColor + ' !important; color: ' + transTextColor + ' !important; } ' +
+                selectorA + ' { color: ' + transATextColor + ' !important; } ' +
+                selectorAVisited + ' { color: ' + transAVisitedTextColor + ' !important; } ' +
+                // и более специфичные селекторы для оригинального лайтспойлера в наведённом состоянии (иначе эти стили не пробиваются через наши)
+                selectorHoverSpoiler + ' { background-color: transparent !important; color: ' + hoverTextColor + ' !important; } ' +
+                selectorHoverA + ' { background-color: transparent !important; color: ' + hoverATextColor + ' !important; } ' +
+                selectorHoverAVisited + ' { background-color: transparent !important; color: ' + hoverAVisitedTextColor + ' !important; } '
+            )
+        } else {
+            this._style = $('<style>').text(
+                selectorPostHoverSpoiler + ' { background-color: ' + transBgColor + ' !important; color: ' + transTextColor + ' !important; } ' +
+                selectorPostHoverA + ' { color: ' + transATextColor + ' !important; } ' +
+                selectorPostHoverAVisited + ' { color: ' + transAVisitedTextColor + ' !important; } ' +
+                // и более специфичные селекторы для оригинального лайтспойлера в наведённом состоянии (иначе эти стили не пробиваются через наши)
+                selectorHoverSpoiler + ' { background-color: transparent !important; color: ' + hoverTextColor + ' !important; } ' +
+                selectorHoverA + ' { background-color: transparent !important; color: ' + hoverATextColor + ' !important; } ' +
+                selectorHoverAVisited + ' { background-color: transparent !important; color: ' + hoverAVisitedTextColor + ' !important; } '
+            )
+        }
+
+    }
+
+    return RevealLiteSpoilersModule
+})
+
 define('shim', [])
 // Т.к. наша урезанная версия define не поддерживает
 // shim конфиг, нужные библиотеки объявим явно.
@@ -899,7 +1001,7 @@ define(['jquery', 'module', 'app', 'cfg-panel-applet'], function($, Module, App,
 
     WhatsNewModule.prototype = new Module()
 
-    WhatsNewModule.prototype.init = function whatsNewModule_init(config, text) {
+    WhatsNewModule.prototype.init = function whatsNew_init(config, text) {
         config = config || {}
 
         if (config.text != text) {
@@ -911,7 +1013,7 @@ define(['jquery', 'module', 'app', 'cfg-panel-applet'], function($, Module, App,
         return config
     }
 
-    WhatsNewModule.prototype.attach = function whatsNewModule_apply(config) {
+    WhatsNewModule.prototype.attach = function whatsNew_apply(config) {
         if (this._alertText) {
             alert(this._alertText)
         }
@@ -919,11 +1021,11 @@ define(['jquery', 'module', 'app', 'cfg-panel-applet'], function($, Module, App,
         delete this._alertText
     }
 
-    WhatsNewModule.prototype.detach = function whatsNewModule_detach() {
+    WhatsNewModule.prototype.detach = function whatsNew_detach() {
         return true
     }
 
-    WhatsNewModule.prototype.createCfgPanelApplet = function whatsNewModule_createCfgPanelApplet() {
+    WhatsNewModule.prototype.createCfgPanelApplet = function whatsNew_createCfgPanelApplet() {
         return new WhatsNewCfgPanelApplet()
     }
 
@@ -931,11 +1033,11 @@ define(['jquery', 'module', 'app', 'cfg-panel-applet'], function($, Module, App,
 
     WhatsNewCfgPanelApplet.prototype = new CfgPanelApplet()
 
-    WhatsNewCfgPanelApplet.prototype.build = function whatsNewCfgPanelApplet_build() {
+    WhatsNewCfgPanelApplet.prototype.build = function whatsNewApplet_build() {
         return this.div = $('<div>')
     }
 
-    WhatsNewCfgPanelApplet.prototype.setData = function whatsNewCfgPanelApplet_setData(enabled, config) {
+    WhatsNewCfgPanelApplet.prototype.setData = function whatsNewApplet_setData(enabled, config) {
         CfgPanelApplet.prototype.setData.apply(this, arguments) // call to super()
         this.div.html('<strong>Что нового:</strong><br/>' + config.text)
     }
@@ -959,8 +1061,9 @@ define(['app'], function(App) {
 
     new App('tabun-fixes')
         .add('cfg-panel',              { defaultEnabled:true })
-        .add('alter-links-to-mirrors', { defaultEnabled:true, cfgPanel:{column:1} })
-        .add('whats-new',              { defaultEnabled:true, cfgPanel:{column:2} },
+        .add('alter-links-to-mirrors', { defaultEnabled:true,  cfgPanel:{column:1} })
+        .add('reveal-lite-spoilers',   { defaultEnabled:false, cfgPanel:{column:1} })
+        .add('whats-new',              { defaultEnabled:true,  cfgPanel:{column:2} },
             "• Новый модульный движок<br/>• Совместимость с новым Табуном"
         )
         //.add('fix-scroll',             { defaultEnabled:true, cfgPanel:{skip:true} })
